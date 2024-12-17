07c911971a58f2c6e27692d38145bfa117a61b62
YS-RAPTOR <yashan.sumanaratne@gmail.com>
2024-12-17 17:56:00 +1100

    Finished

--- app/customer/share/[id].tsx
+++ app/customer/share/[id].tsx
@@ -1,5 +1,6 @@
 import { useLocalSearchParams } from "expo-router";
-import { View } from "react-native";
+import { useState } from "react";
+import { Text, View } from "react-native";
 import QRCode from "react-native-qrcode-svg";
 
 export default function Share() {
@@ -14,7 +15,7 @@ export default function Share() {
             }}
         >
             {/* @ts-ignore */}
-            <QRCode value={id} />
+            <QRCode value={id} size={250} quietZone={25} />
         </View>
     );
 }
